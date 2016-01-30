21264c10449299ed70f7b1f79c1d6316a3932b75
Alex Malcolm <alexmalcolm93@live.co.uk>
2016-01-30 16:33:26 +0000

    added menu
    added menu screen
    added escape functionality to main scene - press escape button on
    keyboard

---
+++ GlobalGameJam2016/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+}
